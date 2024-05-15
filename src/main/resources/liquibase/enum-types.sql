4cae6bae4eb136477fee0fe085b96f7130d48dcb
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-05-16 01:35:07 +0300

    Update enum-types.sql

--- src/main/resources/liquibase/enum-types.sql
+++ src/main/resources/liquibase/enum-types.sql
@@ -1,7 +1,17 @@
 --liquibase formatted sql
 
 --changeset softserveinc:1513597385307-1
-CREATE TYPE IMAGE_TYPE AS ENUM ('USER', 'ISSUE');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'image_type') THEN
+        CREATE TYPE IMAGE_TYPE AS ENUM ('USER', 'ISSUE');
+    END IF;
+END $$;
 
 --changeset softserveinc:1513597385307-2
-CREATE TYPE USER_TYPE AS ENUM ('ROLE_BANNED', 'ROLE_USER', 'ROLE_ADMIN', 'ROLE_MASTER');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_type') THEN
+        CREATE TYPE USER_TYPE AS ENUM ('ROLE_BANNED', 'ROLE_USER', 'ROLE_ADMIN', 'ROLE_MASTER');
+    END IF;
+END $$;
